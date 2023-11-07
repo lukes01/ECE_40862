eac26ec6ba6b792d2601761513197030f50064fd
Lucas Stein <62573194+lukes01@users.noreply.github.com>
2023-11-07 00:47:36 -0500

    Create espclient.py

---
+++ Lab_4_A_Simple_IoT_Application_using_an_ESP32-based_HTTP_Web_Server_and_Client/espclient.py
@@ -0,0 +1,45 @@
+import machine, network, socket, esp32, socket
+from machine import Timer
+from esp32 import raw_temperature
+
+numRan = 0
+
+def print_Hall_Temp(tim):
+    global numRan
+    temp = esp32.raw_temperature()
+    hall = esp32.hall_sensor()
+    print('Temperature:' + str(temp))
+    print('Hall Sensor:' + str(hall) + '\n')
+    sock = socket.socket()
+    addr = socket.getaddrinfo("api.thingspeak.com",80)[0][-1]
+    sock.connect(addr)
+    host = "api.thingspeak.com"
+    path = "api_key=AKJ923LH4M3O3IUX&field1="+str(temp)+"&field2="+str(hall)
+    sock.send(bytes("GET /update?%s HTTP/1.0\r\nHost: %s\r\n\r\n" %(path,host),"utf8"))
+    numRan += 1
+    sock.close()
+    
+    
+def connectWifi():
+    ssid = 'Rise Resident'
+    pword = 'Boilermakers*100'
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.scan()
+    if not wlan.isconnected():
+        wlan.connect(ssid, pword)
+        while not wlan.isconnected():
+            pass
+    print('Connected to', wlan.config('ssid'))
+    print('IP Address:', wlan.ifconfig()[0])
+
+
+connectWifi()
+
+tim = Timer(0)
+tim.init(mode=Timer.PERIODIC, period=30000, callback=print_Hall_Temp)
+
+while numRan < 10:
+    pass
+
+tim.deinit()
