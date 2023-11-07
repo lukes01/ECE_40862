69fa17d92e6cdbb2b975c33d8fcb20afcb3ab128
Lucas Stein <62573194+lukes01@users.noreply.github.com>
2023-11-07 00:32:53 -0500

    Create program1.py

---
+++ Lab_1_Introduction_to_Python_and_Digital_Input_and_Output_using_GPIOs/Part_1/program1.py
@@ -0,0 +1,5 @@
+name = input('What is your name? ')
+age = input('How old are you? ')
+yearTo100 = 100 - int(age)
+centYear = 2023 + yearTo100
+print(name + ' will be 100 years old in the year ' + str(centYear))
